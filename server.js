0415cf43d2f6b774aec862ee4b6db4759e5cf266
Kyle Smelter <kylesmelter@gmail.com>
2022-10-29 14:03:19 -0700

    updated port

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.get('/api/:name', (request, response) => {
     response.json(rappers);
 })
 
-app.listen(PORT, () => {
+app.listen(process.env.PORT || PORT, () => {
     console.log(`The server is now running on port ${PORT}! Better go catch it!`)
 });
 
